d3bcc82359612f923b790198eb11a5c565aa35f6
Vinod VV <vinovijayan@gmail.com>
2025-10-05 22:17:06 +0530

    Get Exchange rates from exchangerate-api

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import csv
 import os
+import requests
 import smtplib
 from email.message import EmailMessage
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 email_address = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
+
+api_key = os.getenv("API_KEY")
+
+url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/GBP"
+
+response = requests.get(url)
+data = response.json()
+
+rate = data["conversion_rates"].get("INR")
+
+date_utc_str = data["time_last_update_utc"]
+
+date_obj = datetime.strptime(date_utc_str, "%a, %d %b %Y %H:%M:%S %z")
+
+formatted_date = date_obj.strftime("%Y-%m-%d")
+
+with open("exchange_rates.csv", "a", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow([formatted_date, rate])
 
 
 with open("exchange_rates.csv") as file:
