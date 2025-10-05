34f645b1795e8ef9e232d0f091dd55704aa342d6
Vinod VV <vinovijayan@gmail.com>
2025-10-05 21:53:27 +0530

    Send email

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import csv
+import os
 import smtplib
 from email.message import EmailMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+email_address = os.getenv("EMAIL_ADDRESS")
+email_password = os.getenv("EMAIL_PASSWORD")
 
 
 with open("exchange_rates.csv") as file:
@@ -11,8 +18,12 @@ with open("exchange_rates.csv") as file:
 for rate in exchange_rates:
     msg = EmailMessage()
     msg['Subject'] = "Today's GBP INR Exchange Rate"
-    msg['From'] = "vinovijayan@gmail.com"
-    msg['To'] = "vinovijayan@gmail.com"
+    msg['From'] = "Vinod VV"
+    msg['To'] = "vinovijayan@hotmail.com"
     msg.set_content(f"Exchange Rate (GBP-INR): {rate['GBP_INR_Rate']}\n"
                     f"Date: {rate['date']}\n ")
-    print(msg)
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+        smtp.starttls()
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
