aec3ba35cd723429f73a88b7054210bc97da8d45
Vinod VV <vinovijayan@gmail.com>
2025-10-05 22:44:17 +0530

    Optimized main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ load_dotenv()
 
 email_address = os.getenv("EMAIL_ADDRESS")
 email_password = os.getenv("EMAIL_PASSWORD")
-
 api_key = os.getenv("API_KEY")
 
 url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/GBP"
@@ -26,25 +25,26 @@ date_obj = datetime.strptime(date_utc_str, "%a, %d %b %Y %H:%M:%S %z")
 
 formatted_date = date_obj.strftime("%Y-%m-%d")
 
-with open("exchange_rates.csv", "a", newline="") as file:
+with open("exchange_rates.csv", "a") as file:
     writer = csv.writer(file)
     writer.writerow([formatted_date, rate])
-
 
 with open("exchange_rates.csv") as file:
     reader = csv.DictReader(file)
     exchange_rates = list(reader)
 
+today = datetime.now().strftime("%Y-%m-%d")
 
-for rate in exchange_rates:
-    msg = EmailMessage()
-    msg['Subject'] = "Today's GBP INR Exchange Rate"
-    msg['From'] = "Vinod VV"
-    msg['To'] = "vinovijayan@hotmail.com"
-    msg.set_content(f"Exchange Rate (GBP-INR): {rate['GBP_INR_Rate']}\n"
-                    f"Date: {rate['date']}\n ")
+with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
+    smtp.starttls()
+    smtp.login(email_address, email_password)
 
-    with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
-        smtp.starttls()
-        smtp.login(email_address, email_password)
-        smtp.send_message(msg)
+    for rate in exchange_rates:
+        if rate['date'] == today:
+            msg = EmailMessage()
+            msg['Subject'] = "Today's GBP INR Exchange Rate"
+            msg['From'] = "Vinod VV"
+            msg['To'] = "vinovijayan@hotmail.com"
+            msg.set_content(f"Exchange Rate (GBP-INR): {rate['GBP_INR_Rate']}\n"
+                            f"Date: {rate['date']}\n ")
+            smtp.send_message(msg)
