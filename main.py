6a03e389cd70be6ce03bc93f5427cc0fa2d59b5e
Vinod VV <vinovijayan@gmail.com>
2025-10-05 15:12:14 +0530

    Create email message

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import csv
+import smtplib
+from email.message import EmailMessage
+
 
 with open("exchange_rates.csv") as file:
     reader = csv.DictReader(file)
     exchange_rates = list(reader)
 
 
-print(exchange_rates)
-
+for rate in exchange_rates:
+    msg = EmailMessage()
+    msg['Subject'] = "Today's GBP INR Exchange Rate"
+    msg['From'] = "vinovijayan@gmail.com"
+    msg['To'] = "vinovijayan@gmail.com"
+    msg.set_content(f"Exchange Rate (GBP-INR): {rate['GBP_INR_Rate']}\n"
+                    f"Date: {rate['date']}\n ")
+    print(msg)
